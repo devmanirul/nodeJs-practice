93ce68dd8342603a6764bfefd22a0843425dabc4
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2025-05-05 23:43:00 +0600

    frist commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import http from 'http'
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
+  })
+  .listen(5000);
