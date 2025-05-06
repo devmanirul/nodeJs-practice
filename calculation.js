67e73d49c85eec1ae05f05a9871cad8e5819dff8
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2025-05-06 23:31:00 +0600

    nodemon add

---
+++ calculation.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+const calculation = { sum, sub };
+export default calculation;
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-import http from 'http'
-var http = require("http");
+//
+import dateTime from "./dateTime.js";
+
+import http from "http";
 
 http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
+  .createServer((req, res) => {
+    const path = req.url;
+    req.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    if (path === "/") {
+      res.end("<h2>Server Homepage</h2>");
+    } else if (path === "/about") {
+      res.end("<h1>Server About Page</h1>");
+    }
   })
   .listen(5000);
